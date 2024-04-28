a1629ffda224a612cd919431430b6135189a56af
Željko Brcković <Zeljko.Brckovic1@infobip.com>
2024-04-28 20:42:29 +0200

    .

---
+++ src/permutation-encoder.ts
@@ -0,0 +1,14 @@
+export const encodePermutation = (permutation: number[]): number => {
+    const n = permutation.length
+    if (n <= 1) return 0
+    const [first, ...rest] = permutation
+    return first * factorial(n - 1) + encodePermutation(rest.map(x => x < first ? x : x - 1))
+}
+
+const factorial = (n: number): number => {
+    let result = 1
+    for (let i = 2; i <= n; i++) {
+        result *= i
+    }
+    return result
+}

901234a42873c15d93daa612e9bec30f20d9dd67
Željko Brcković <Zeljko.Brckovic1@infobip.com>
2024-04-29 13:58:49 +0200

    .

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,9 +1,13 @@
 import React, { FC, useCallback, useEffect, useState } from 'react'
+import { PlayIcon, StopIcon, PauseIcon, ArrowUturnLeftIcon } from '@heroicons/react/24/solid'
 
 const CANVAS_WIDTH = 1024
 const CANVAS_HEIGHT = 768
 
 export const Page: FC = () => {
+    const [playing, setPlaying] = useState(false)
+    const [dotCount, setDotCount] = useState(0)
+
     const [ctx, setCtx] = useState<CanvasRenderingContext2D>()
 
     const clearCanvas = useCallback(() => {
@@ -14,34 +18,59 @@ export const Page: FC = () => {
 
     const drawPixelDot = useCallback((x: number, y: number) => {
         if (ctx === undefined) return
-        ctx.fillStyle = "rgba(255,0,0,0.5)";
+        ctx.fillStyle = 'rgba(255,0,0,0.5)'
         ctx.fillRect(x, y, 1, 1)
     }, [ctx])
 
     useEffect(() => {
         if (ctx === undefined) return
 
-        clearCanvas()
+        if (!playing) return
 
         const intervalId = setInterval(() => {
             const x = Math.floor(Math.random() * CANVAS_WIDTH)
             const y = Math.floor(Math.random() * CANVAS_HEIGHT)
             drawPixelDot(x, y)
-        }, 0)
+            setDotCount(prev => prev + 1)
+        })
 
         return () => clearInterval(intervalId)
-    }, [ctx])
+    }, [ctx, playing])
 
-    return <div className="grow p-1">
-        <canvas
-            width={CANVAS_WIDTH}
-            height={CANVAS_HEIGHT}
-            ref={canvas => {
-                if (canvas === null) return
-                const ctx = canvas.getContext('2d')
-                if (ctx === null) return
-                setCtx(ctx)
-            }}
-            className="border-solid border border-black"/>
+    return <div className='grow flex flex-col items-center p-4'>
+        <div className='flex flex-col gap-2'>
+            <div className='flex items-center gap-1'>
+                <select>
+                    
+                </select>
+                <button
+                    className='btn'
+                    onClick={() => setPlaying(prev => !prev)}
+                >
+                    {playing ? <PauseIcon className='w-6' /> : <PlayIcon className='w-6' />}
+                </button>
+                <button className='btn' onClick={() => {
+                    setPlaying(false)
+                    setDotCount(0)
+                    clearCanvas()
+                }}>
+                    <StopIcon className='w-6' />
+                </button>
+                <div className='ms-auto'>
+                    <label className='font-bold'>Dot count:</label> <span>{dotCount}</span>
+                </div>
+            </div>
+            <canvas
+                className='border-solid border border-black'
+                width={CANVAS_WIDTH}
+                height={CANVAS_HEIGHT}
+                ref={canvas => {
+                    if (canvas === null) return
+                    const ctx = canvas.getContext('2d')
+                    if (ctx === null) return
+                    setCtx(ctx)
+                }}
+            />
+        </div>
     </div>
 }
